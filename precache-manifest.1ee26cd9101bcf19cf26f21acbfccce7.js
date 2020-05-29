155cf7e759af06dd8e098640e45b5baa635289a5
qb-athirau <62994344+qb-athirau@users.noreply.github.com>
2020-05-29 17:18:03 +0530

    Updates

---
+++ precache-manifest.1ee26cd9101bcf19cf26f21acbfccce7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d9c94a12fa1099f05ea2ba99e3f09b",
+    "url": "/hospital-management/index.html"
+  },
+  {
+    "revision": "3be348516e7015015f06",
+    "url": "/hospital-management/static/css/main.e43c7a89.chunk.css"
+  },
+  {
+    "revision": "3b780a906faa681f209d",
+    "url": "/hospital-management/static/js/0.f95a3edd.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/hospital-management/static/js/0.f95a3edd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359a096ec6587a7cb950",
+    "url": "/hospital-management/static/js/1.72fe79c4.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/hospital-management/static/js/1.72fe79c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098383a9d21bd47a6a61",
+    "url": "/hospital-management/static/js/4.b29ea7e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hospital-management/static/js/4.b29ea7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cc371258a3a41534f7",
+    "url": "/hospital-management/static/js/5.3a407558.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/hospital-management/static/js/5.3a407558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050844fe99536c0a00bc",
+    "url": "/hospital-management/static/js/6.c9528324.chunk.js"
+  },
+  {
+    "revision": "36e0e50f6e83dc4ec717",
+    "url": "/hospital-management/static/js/7.5ddf5394.chunk.js"
+  },
+  {
+    "revision": "46c36ad6faa7dd5cc45d",
+    "url": "/hospital-management/static/js/8.4521cd95.chunk.js"
+  },
+  {
+    "revision": "6bcaf7de5743b5c689b2",
+    "url": "/hospital-management/static/js/9.242c1f11.chunk.js"
+  },
+  {
+    "revision": "3be348516e7015015f06",
+    "url": "/hospital-management/static/js/main.9a2c3540.chunk.js"
+  },
+  {
+    "revision": "cde3f2200d49fa3d4f52",
+    "url": "/hospital-management/static/js/runtime-main.8a9bf3f4.js"
+  },
+  {
+    "revision": "cdd83730878c549a9e9a754cb6dea2d3",
+    "url": "/hospital-management/static/media/page_not_found.cdd83730.png"
+  }
+]);
